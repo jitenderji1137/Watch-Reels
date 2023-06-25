4511d41ff3de5c262845533e72ba7b1fa32b3435
jitender <trademetrader1137@gmail.com>
2023-06-25 18:32:31 +0530

    Watch

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Iframe from "./Iframe"
+function App() {
+  return (<>
+  <Iframe/>
+  </>);
+}
+
+export default App;
---
+++ src/Iframe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Iframe = () => {
+    return (
+    <iframe
+        src="https://media.chingari.io/uploads/648fdf1a08ea789c6dfe2d56/transcode/720p/h264_720p26cbc1bc-f75e-4160-a722-828bff479c69-1687150343171-TXVideo_20230619_102208.mp4"
+        title="Embedded Content"
+        style={{position: 'absolute',top: 0,left: 0,width: '100%',height: '100%',border: 0,}}
+        allowFullScreen
+    />
+);
+};
+
+export default Iframe;
