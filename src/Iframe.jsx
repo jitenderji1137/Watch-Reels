247ef9262a42412edeb42ae19d74fdc2f9665081
jitender <trademetrader1137@gmail.com>
2023-06-26 03:15:04 +0530

    watch

--- src/Iframe.jsx
+++ src/Iframe.jsx
@@ -1,9 +1,10 @@
-import React,{useEffect, useState} from 'react';
+import React,{useEffect,useState} from 'react';
 import axios from 'axios';
 import ReactPlayer from 'react-player'
 const Iframe = () => {
     const [videoarr,videoarrvalue] = useState([]);
     const [count,countvalue] = useState(0);
+    const [val,valvalue] = useState(true);
     function getdata(){
         axios.post('https://api.chingari.io/post/trending-video-current?skip=0&limit=10&language=english')
         .then(function (response) {
@@ -15,12 +16,14 @@ const Iframe = () => {
                     ar.push(ardata.p1024);
                 }
             }
-            videoarrvalue(...videoarr,ar);
+            videoarrvalue([...videoarr,...ar]);
+            valvalue(false)
         })
     }
-    useEffect(()=>{
+    useEffect(() => {
         getdata();
-    },[]);
+    }, []);
+    useEffect(() => {}, [val]);
     const handlestate = () => {
         countvalue(count+1);
         if(videoarr.length-2 === count){
@@ -29,7 +32,8 @@ const Iframe = () => {
     };
     return (
         <>
-        <ReactPlayer width="100%" height="100vh" muted={false} playing={true} loop={true} url={`https://media.chingari.io${videoarr[count]}`}/>    
+        {count>0?
+        <ReactPlayer width="100%" height="100vh" muted={false} playing={true} loop={true} url={`https://media.chingari.io${videoarr[count]}`}/>:<></>}  
         <button style={{position:"absolute",top:"0",background:"transparent",width:"100vw",height:"50vh",border: "none",borderColor:"transparent"}} onClick={()=>{if(count>0){countvalue(count-1);}}}></button>
         <button style={{position:"absolute",bottom:"0",background:"transparent",width:"100vw",height:"50vh",border: "none",borderColor:"transparent"}} onClick={handlestate}></button>
         </>
