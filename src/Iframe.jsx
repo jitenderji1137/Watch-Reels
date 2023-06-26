d9f2ba3ba7c95b7c38ce7ac47eece3152f03b837
jitender <trademetrader1137@gmail.com>
2023-06-26 18:27:45 +0530

    Watch

--- src/Iframe.jsx
+++ src/Iframe.jsx
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player'
 const Iframe = () => {
     const [videoarr,videoarrvalue] = useState([]);
     const [count,countvalue] = useState(0);
-    const [val,valvalue] = useState(true);
+    const [vall,vvalvalue] = useState(true);
     function getdata(){
         axios.post('https://api.chingari.io/post/trending-video-current?skip=0&limit=10&language=english')
         .then(function (response) {
@@ -17,25 +17,23 @@ const Iframe = () => {
                 }
             }
             videoarrvalue([...videoarr,...ar]);
-            valvalue(false)
         })
     }
     useEffect(() => {
         getdata();
     }, []);
-    useEffect(() => {}, [val]);
     const handlestate = () => {
         countvalue(count+1);
         if(videoarr.length-2 === count){
             getdata();
         }
     };
-    return (
+        return (
         <>
         {count>=0?
         <ReactPlayer width="100%" height="100vh" muted={false} playing={true} loop={true} url={`https://media.chingari.io${videoarr[count]}`}/>:<></>}  
-        <button style={{position:"absolute",top:"0",background:"transparent",width:"100vw",height:"50vh",border: "none",borderColor:"transparent"}} onClick={()=>{if(count>0){countvalue(count-1);}}}></button>
-        <button style={{position:"absolute",bottom:"0",background:"transparent",width:"100vw",height:"50vh",border: "none",borderColor:"transparent"}} onClick={handlestate}></button>
+        <button style={{position:"absolute",top:"0",background:"transparent",width:"100vw",height:"50vh",border: "none",borderColor:"transparent",outline:"none"}} onClick={()=>{if(count>0){countvalue(count-1);}}}></button>
+        <button style={{position:"absolute",bottom:"0",background:"transparent",width:"100vw",height:"50vh",border: "none",borderColor:"transparent",outline:"none"}} onClick={handlestate}></button>
         </>
     
 );
